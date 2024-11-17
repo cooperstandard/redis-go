1f33e60fca87a59556aa1b6a85b29299412749c2
Cooper Standard <cooper@cooperstandard.org>
2024-11-17 12:22:53 -0800

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -34,12 +34,12 @@ func main() {
 }
 func handleConnection(conn net.Conn) {
 	b := make([]byte, 128)
-	_, err := conn.Read(b)
+	n, err := conn.Read(b)
 	if err != nil {
 		fmt.Println("Error reading connection: ", err.Error())
 		os.Exit(1)
 	}
-	fmt.Println("Message received: ", b)
+	fmt.Println("Message received: ", b[:n])
 	_, err = conn.Write([]byte("+PONG\r\n"))
 	if err != nil {
 		fmt.Println("Error writing connection: ", err.Error())
